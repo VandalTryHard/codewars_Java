47cc4a9ad8b8d63448c458d9fc8f266bf10ba3ef
Val <gulenok.valentin@yandex.ru>
2021-11-08 06:39:39 +0600

    solution codewars.com Java
    8 kyu

---
+++ kyu8/OppositeNumber.java
@@ -0,0 +1,31 @@
+/*8 kyu
+Opposite number
+Very simple, given a number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+*/
+
+// public class Kata
+//     {
+//         public static int opposite(int number)
+//         {
+//             return number*-1;
+//         }
+//     }
+
+//How this works
+package kyu8;
+
+public class OppositeNumber {
+    public static void main(String[] args){
+        int n = 1;
+        int x = -1;
+        System.out.println(n*-1);
+        System.out.println(x*-1);
+    }
+    
+}
